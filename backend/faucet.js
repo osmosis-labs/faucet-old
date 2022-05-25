a26caa1f4aad1e347e18f8a52bd82d4f0acc3c73
Daniel Farina <contact@nevulas.com>
2022-05-25 15:54:49 -0400

    backend removed commas

--- backend/faucet.js
+++ backend/faucet.js
@@ -152,8 +152,8 @@ function secondsToHms(d) {
     let m = Math.floor(d % 3600 / 60);
     let s = Math.floor(d % 3600 % 60);
 
-    let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours ") : "";
+    let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes ") : "";
     let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
     return hDisplay + mDisplay + sDisplay;
 }
