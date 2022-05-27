260a76c60bef7206fe1cfefe927ee2e1abd04967
Daniel Farina <contact@nevulas.com>
2022-05-27 12:36:41 -0400

    Updating deps

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import App from './App.vue'
