7907bac53da0d9ca120a0b6cc8f0b9a2d5246db2
Niccolo Raspa <nraspa@live.it>
2022-05-31 15:09:14 +0200

    Update docker entrypoint

--- frontend/docker/docker-entrypoint.sh
+++ frontend/docker/docker-entrypoint.sh
@@ -4,10 +4,10 @@ set -eu
 # Replace PORT in nginx configuration
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Replace FAUCET_SERVER if not empty
+# Set FAUCET_SERVER variable dinamically if not empty
 if [ ! -z "$FAUCET_SERVER" ]
 then
-    sed -i -r "s#window.FAUCET_SERVER = null;#window.FAUCET_SERVER = $FAUCET_SERVER;#" /usr/share/nginx/html/config.js
+    sed -i -r "s#window.FAUCET_SERVER = null;#window.FAUCET_SERVER = \"$FAUCET_SERVER\";#" /usr/share/nginx/html/config.js
 fi
 
 exec "$@"
