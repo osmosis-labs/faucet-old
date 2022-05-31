f5a0215d747d2458fd2959afa42664b2f7901525
Daniel Farina <contact@nevulas.com>
2022-05-31 13:30:19 -0400

    Updated bootstrap library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import 'animate.css';
 import App from './App.vue'
