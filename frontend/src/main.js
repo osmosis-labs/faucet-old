abbb8637338981a5ca2fdd133f13eade59994d79
Daniel Farina <contact@nevulas.com>
2022-05-31 13:27:32 -0400

    Add missing bootstrap js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import 'animate.css';
 import App from './App.vue'
