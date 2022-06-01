30211313fc6ebefb44e9cd32d961128b558d37e6
Daniel Farina <contact@nevulas.com>
2022-06-01 13:21:12 -0400

    adding buffer package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
+import buffer from 'buffer'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
