d095a0d07843c8a99aa9425e2d45ea2734bd45fa
Daniel Farina <contact@nevulas.com>
2022-08-04 20:29:48 -0700

    Updated some gas settings
    Just added more gas for Cosmwasm transactions. We should estimate gas soon. Not high priority for now.

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -1,14 +1,14 @@
 
 const FAUCET_QUEUE_LIMIT = 15;
-const AMOUNT = "500000000";
+const AMOUNT = "100000000";
 const DENOM = "uosmo";
 const CHAIN_ID = "osmo-testnet-4";
 let faucetQueue=[];
 const prefix= "osmo";
-const gas_price = "0uosmo";
-const gas = "500000";
+const gas_price = "0.25uosmo";
+const gas = "20000000";
 const TIME_LIMIT = 3600 * 2; // 3600 seconds x 2 = 2 hours
-const MAX_PER_IP = 2;
+const MAX_PER_IP = 100;
 const MAX_VOTE_PER_IP = 20;
 const HD_PATH = "m/44'/118'/0'/0/0";
 module.exports = {
