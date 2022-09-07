381105347d84acb6f0b8e253ea511927d04a7417
Niccolo Raspa <nraspa@live.it>
2022-09-07 16:21:08 +0200

    Use VITE_ for all environment variables

--- frontend/docker/docker-entrypoint.sh
+++ frontend/docker/docker-entrypoint.sh
@@ -5,9 +5,9 @@ set -eu
 envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Set FAUCET_BACKEND variable dinamically if not empty
-if [ ! -z "$FAUCET_BACKEND" ]
-then
-    sed -i -r "s#window.FAUCET_BACKEND = null;#window.FAUCET_BACKEND = \"$FAUCET_BACKEND\";#" /usr/share/nginx/html/config.js
-fi
+# if [ ! -z "$FAUCET_BACKEND" ]
+# then
+#     sed -i -r "s#window.FAUCET_BACKEND = null;#window.FAUCET_BACKEND = \"$FAUCET_BACKEND\";#" /usr/share/nginx/html/config.js
+# fi
 
 exec "$@"
--- frontend/src/stores/faucet.js
+++ frontend/src/stores/faucet.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import { config } from '@/config'
 
 // main is the name of the store. It is unique across your application
 // and will appear in devtools
@@ -7,7 +8,7 @@ export const useFaucetStore = defineStore('faucet', {
     state: () => ({
         form: {
             formName: "Osmosis testnet faucet",
-            endpoint: window.FAUCET_BACKEND ? window.FAUCET_BACKEND : import.meta.env.VITE_FAUCET_BACKEND,
+            endpoint: config.FAUCET_BACKEND,
             payload: {
                 mnemonic: "",
                 address: "",
