ff3f444a6f63a8ed7f2de153cf9bc9ce717de1fe
Niccolo Raspa <nraspa@live.it>
2023-03-07 15:57:21 +0100

    Add fund echo method

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
-from flask import render_template
+from flask import request, jsonify, render_template
 
 app = Flask(__name__, static_url_path='/static')
+
+@app.route('/fund', methods=['GET', 'POST'])
+def fund():
+    data = request.json
+    print(data)
+    return jsonify(data)
 
 @app.route("/")
 def index():
