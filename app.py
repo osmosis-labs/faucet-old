448d760f8791089f53e53f1cf6487ea4356d6452
Niccolo Raspa <nraspa@live.it>
2023-03-07 16:54:01 +0100

    Add /fund method

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import request, jsonify, render_template
+import redis
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/fund', methods=['GET', 'POST'])
+@app.route('/fund', methods=['GET'])
 def fund():
-    data = request.json
-    print(data)
-    return jsonify(data)
+    address = request.args.get('address')
+    mock_data = {
+        'address': address,
+        'amount': '100 OSMO'
+    }
+    return jsonify(mock_data)
 
 @app.route("/")
 def index():
