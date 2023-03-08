afe92ccd0c5350e02f2279daad77ede7226f2389
Niccolo Raspa <nraspa@live.it>
2023-03-08 15:41:34 +0100

    More cosmetic changes

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,18 @@ $(document).ready(function() {
 
         var address = $('#address-input').val(); // get the address input value
 
+        // show spinning wheel on submit button
+        $('#faucet-form button[type="submit"]').html('<i class="fa fa-spinner fa-spin"></i> Request tokens');
+
         // send AJAX request to the server
         $.ajax({
             url: '/fund',
             data: { address: address },
             type: 'GET',
+            beforeSend: function() {
+                // disable submit button while request is being processed
+                $('#faucet-form button[type="submit"]').prop('disabled', true);
+            },
             success: function(response) {
                 console.log(response);
                 $('#success-alert').text(response.message).removeClass('d-none'); // show success alert with message
@@ -19,6 +26,10 @@ $(document).ready(function() {
                 var errorMessage = jqXHR.responseJSON.error.replace("400 Bad Request: ", "");
                 $('#error-alert').text(errorMessage).removeClass('d-none'); // show error alert with message
                 $('#success-alert').addClass('d-none'); // hide success alert
+            },
+            complete: function() {
+                // re-enable submit button when request is complete
+                $('#faucet-form button[type="submit"]').prop('disabled', false).html('Request tokens');
             }
         });
     });
