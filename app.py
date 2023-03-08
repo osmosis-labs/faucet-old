f2daab923c3bae1fcafa354842289d77700f02f9
Niccolo Raspa <nraspa@live.it>
2023-03-08 15:29:24 +0100

    Improve error messages

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import logging
 
-
 from flask import Flask
 from flask import request, jsonify, render_template
 from werkzeug.exceptions import BadRequest
@@ -11,9 +10,6 @@ from config.wallet import initialize_wallet
 from config.ledger_client import initialize_ledger_client
 
 app = Flask(__name__, static_url_path='/static')
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
 
 @app.before_first_request
 def run_on_startup():
@@ -49,14 +45,14 @@ def fund():
         osmosis_address = request.args.get('address')
 
         if not osmosis_address:
-            logging.error("Osmosis address is required")
+            app.logger.error("Osmosis address is required")
             raise BadRequest('Osmosis address is required')
 
         if not is_valid_osmosis_address(osmosis_address):
-            logging.error("Osmosis address is invalid")
+            app.logger.error("Osmosis address is invalid")
             raise BadRequest('Osmosis address is invalid')
         
-        logging.info(f"Received funding request for address: {osmosis_address}")
+        app.logger.info(f"Received funding request for address: {osmosis_address}")
 
         # Check 1: Check if the address has already been funded in the last 24 hours
         if redis_client.get(osmosis_address):
@@ -64,10 +60,10 @@ def fund():
         
         # Check 2: Check if the address has already enough uosmo
         balance = ledger_client.query_bank_balance(osmosis_address)
-        logging.debug(f"Address balance: {balance}")
+        app.logger.debug(f"Address balance: {balance}")
 
         if balance >= MAX_FUNDING_AMOUNT:
-            logging.info(f"Address already funded. Balance: {balance}")
+            app.logger.info(f"Address already funded. Balance: {balance}")
             raise BadRequest('Osmosis has already enough tokens')
         
         # Check 3: Check the number of requests made by that IP in the last 24 hours
@@ -77,16 +73,15 @@ def fund():
         # TODO: Implement this
 
         # Fund the address
-        logging.debug("Sending funds to address...")
+        app.logger.debug("Sending funds to address...")
         tx = ledger_client.send_tokens(osmosis_address, FUNDING_AMOUNT, "uosmo", wallet)
-        logging.debug("Funds sent.")
+        app.logger.debug("Funds sent.")
 
         # block until the transaction has been successful or failed
-        logging.debug("Waiting for transaction...")
+        app.logger.debug("Waiting for transaction...")
         tx.wait_to_complete()
-        logging.info(f"Successfully funded Osmosis address: {osmosis_address}")
+        app.logger.info(f"Successfully funded Osmosis address: {osmosis_address}")
         
-
         # Mark the address as funded in Redis
         redis_client.setex(osmosis_address, 86400, 'funded')
 
--- static/js/script.js
+++ static/js/script.js
@@ -11,12 +11,13 @@ $(document).ready(function() {
             type: 'GET',
             success: function(response) {
                 console.log(response);
-                $('#success-alert').removeClass('d-none'); // show success alert
+                $('#success-alert').text(response.message).removeClass('d-none'); // show success alert with message
                 $('#error-alert').addClass('d-none'); // hide error alert
             },
             error: function(jqXHR, textStatus, errorThrown) {
-                console.log(jqXHR);
-                $('#error-alert').removeClass('d-none'); // show error alert
+                console.log(jqXHR.responseJSON);
+                var errorMessage = jqXHR.responseJSON.error.replace("400 Bad Request: ", "");
+                $('#error-alert').text(errorMessage).removeClass('d-none'); // show error alert with message
                 $('#success-alert').addClass('d-none'); // hide success alert
             }
         });
